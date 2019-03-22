490662e44f50945de74a7d11f6dd3515b17e3e54
Jayam Shah <jshah59@illinois.edu>
2019-03-22 13:55:48 -0500

    Finished initial writeup of models

--- wersocool/core/models.py
+++ wersocool/core/models.py
@@ -27,7 +27,24 @@ class RSO(models.Model):
     college_assocation = models.CharField(max_length=50)
     picture_url = models.ImageField(upload_to='rso_images/')
 
-#class InvolvedWith(models.Model):
-#    member_netId = models.OneToOne(Member, on_delete=models.CASCADE)
-#    rso_name = models.OneToOne(RSO, on_delete=models.CASCADE)
-    
+class InvolvedWith(models.Model):
+    member_netId = models.ForeignKey(Member, on_delete=models.CASCADE)
+    rso_name = models.ForeignKey(RSO, on_delete=models.CASCADE)
+    role = models.CharField(max_length=80)
+    semesters_involved = models.IntegerField()
+
+    class Meta:
+        unique_together = (("member_netId", "rso_name"))
+
+class MeetingEvent(models.Model):
+    name = models.CharField(max_length=100)
+    date_time = models.DateTimeField()
+    rso_name = models.ForeignKey(RSO, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = (("name", "rso_name"))
+
+class Attends(models.Model):
+    member_netId = models.ForeignKey(Member, on_delete=models.CASCADE)
+    meeting_name = models.ForeignKey(MeetingEvent, on_delete=models.CASCADE)
+    rso_name = models.ForeignKey(RSO, on_delete=models.CASCADE)
