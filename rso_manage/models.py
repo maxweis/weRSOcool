37650e2a33483e756614e8a15da2f320338ce823
ishan909 <ishan909@gmail.com>
2019-03-22 17:39:40 -0500

    updated rso_manage models and included as installed app

--- rso_manage/models.py
+++ rso_manage/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class RSO(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=100)
     year_established = models.IntegerField()
-    college_association = models.CharField()
+    college_association = models.CharField(max_length=50)
     icon = models.ImageField()
--- wersocool/settings.py
+++ wersocool/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'crispy_forms',
     'users.apps.UsersConfig',
+    'rso_manage.apps.UsersConfig',
 )
 
 MIDDLEWARE = (
