9846d47b56302b83451eafc30f7514f2e04a87ab
Max Weis <maxrobweis@gmail.com>
2019-03-24 06:56:20 +0000

    default rso creation date to today

--- rso_manage/forms.py
+++ rso_manage/forms.py
@@ -7,7 +7,7 @@ import datetime
 class RSOCreationForm(ModelForm):
     name = forms.CharField(max_length=64)
     now = datetime.datetime.now()
-    date_established = forms.DateField(widget=forms.SelectDateWidget(years=range(1960, int(now.year) + 1)))
+    date_established = forms.DateField(initial=now.today, widget=forms.SelectDateWidget(years=range(1960, int(now.year) + 1)))
 
     college_association = forms.CharField(max_length=64)
     icon = forms.ImageField(required=False)
