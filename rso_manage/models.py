5eb7d4a8caa282001a515e089d6c6ad6650a073e
Max Weis <maxrobweis@gmail.com>
2019-03-25 02:48:31 +0000

    remove usless default

--- rso_manage/models.py
+++ rso_manage/models.py
@@ -5,7 +5,7 @@ class RSO(models.Model):
     name = models.CharField(max_length=100)
     date_established = models.DateField()
     college_association = models.CharField(max_length=50)
-    icon = models.ImageField(upload_to='rso_icons/', default='rso_icons/None/none.jpg')
+    icon = models.ImageField(upload_to='rso_icons/')
     creator = models.CharField(max_length=64)
 
     def __str__(self):
