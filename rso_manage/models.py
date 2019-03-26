e3af3282504514ed4a7a1e2cbcaced3dcc7b047e
Max Weis <maxrobweis@gmail.com>
2019-03-26 03:21:48 -0500

    scale rso images at submit if large images uploaded

--- rso_manage/models.py
+++ rso_manage/models.py
@@ -1,5 +1,14 @@
 from django.db import models
+from django.core.files.base import ContentFile
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from users.models import Member
+from PIL import Image
+from io import BytesIO
+import sys
+
+
+MAX_IMAGE_WIDTH = 1200
+MAX_IMAGE_HEIGHT = 400
 
 class RSO(models.Model):
     name = models.CharField(max_length=100)
@@ -10,3 +19,14 @@ class RSO(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if self.icon:
+            thumbnail = Image.open(self.icon)
+            thumbnail.thumbnail((MAX_IMAGE_WIDTH, MAX_IMAGE_HEIGHT), Image.ANTIALIAS)
+            thumbnail_file = BytesIO()
+            thumbnail.save(thumbnail_file, "PNG")
+            thumbnail_file.seek(0)
+            self.icon = InMemoryUploadedFile(thumbnail_file, 'ImageField', "{}.png".format(self.icon.name.split(".")[0]), 'image/png', sys.getsizeof(thumbnail_file), None)
+        super(RSO, self).save(*args, **kwargs)
+
