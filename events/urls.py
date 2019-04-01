0528ea4e8f1f33f58dabb998325e92b0e6172e3c
Justin Bieber <jzw2@gmail.com>
2019-03-31 22:01:59 -0500

    Added ability for member to attend event
    The first "advanced feature" will be the ability to visualize attendances
    Right now it just prints out who is attending what but it should later become
    a graph of some sorts

--- events/urls.py
+++ events/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('rsos/<rso_name>/add_event', views.AddEvent, name='add_event'),
     path('rsos/<rso_name>/events', views.display_events, name='list_events'),
+    path('rsos/<rso_name>/<event>/attend', views.attend_event, name='attend_event'),
 ]
--- events/views.py
+++ events/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import EventCreationForm
 from rso_manage.models import RSO
+from events.models import Event, Attending
+from users.models import Member
 
 def AddEvent(request, rso_name):
     event_rso = RSO.objects.get(name=rso_name)
@@ -27,4 +29,25 @@ def display_events(request, rso_name):
     rso = get_object_or_404(RSO, name=rso_name)
     rso_id = RSO.objects.get(name=rso_name).id
     all_events = RSO.objects.raw('SELECT * FROM "events_event" WHERE rso_id = {}'.format(rso_id))
-    return render(request, 'event_list.html', {'all_events' : all_events, 'rso' : rso})
+    return render(request, 'event_list.html', {'all_events' : all_events, 'rso' : rso, 'attend' : Attending.objects.all()})
+
+def attend_event(request, rso_name, event):
+    event = Event.objects.get(name=event)
+    username = request.user.username
+    member = Member.objects.get(username=username)
+    if not Attending.objects.filter(user=member, event=event).exists():
+        attendance = Attending(user=member, event=event)
+        attendance.save()
+    return redirect('/rsos/'+rso_name+'/events')
+
+
+def event_statistics(request, rso_name):
+    #WIP
+    rso = get_object_or_404(RSO, name=rso_name)
+    rso_id = RSO.objects.get(name=rso_name).id
+    all_events = RSO.objects.raw('SELECT * FROM "events_event" WHERE rso_id = {}'.format(rso_id))
+    attendance = RSO.objects.raw('SELECT id, name, count(user_id) FROM "events_event", "events_attending" WHERE rso_id = {} Group By name '.format(rso_id))
+# list(RSO.objects.raw('SELECT events_event.id, name, count(user_id) FROM "events_event", "events_attending" Group By name '))
+    print(attendance)
+    return None
+    
