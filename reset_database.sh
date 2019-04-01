5dc75e56cb396a397444c86c281a7a90b88ea3f0
Justin Bieber <jzw2@gmail.com>
2019-03-31 21:16:47 -0500

    Updated reset_database script

--- reset_database.sh
+++ reset_database.sh
@@ -2,6 +2,7 @@
 rm ./db.sqlite3
 sudo rm ./user/migrations -R
 sudo rm ./rso_manage/migrations -R
+rm -rf ./events/migrations
 python3 ./manage.py migrate --run-syncdb
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@maxweis.com', 'admin')" | python3 ./manage.py shell
 
