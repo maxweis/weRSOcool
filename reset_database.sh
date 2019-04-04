6510f3f175cae6bfc7786590bc54ca93d82161f2
Max Weis <maxrobweis@gmail.com>
2019-04-04 09:52:21 -0500

    update db reset script for new app

--- reset_database.sh
+++ reset_database.sh
@@ -7,6 +7,6 @@ rm -rf ./wersocool/__pycache__
 rm -rf ./users/__pycache__
 rm -rf ./events/__pycache__
 rm -rf ./rso_manage/__pycache__
-python3 ./manage.py makemigrations users rso_manage events
+python3 ./manage.py makemigrations users rso_manage events analytics
 python3 ./manage.py migrate --run-syncdb
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@maxweis.com', 'admin')" | python3 ./manage.py shell
