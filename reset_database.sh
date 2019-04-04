4c216f17f87e4fe2bf98d6dca78106e4d47fa63a
Max Weis <maxrobweis@gmail.com>
2019-04-04 04:56:26 -0500

    fix reset script

--- reset_database.sh
+++ reset_database.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 rm ./db.sqlite3
-sudo rm ./user/migrations -R
-sudo rm ./rso_manage/migrations -R
+rm -rf ./users/migrations
+rm -rf ./rso_manage/migrations
 rm -rf ./events/migrations
+rm -rf ./wersocool/__pycache__
+rm -rf ./users/__pycache__
+rm -rf ./events/__pycache__
+rm -rf ./rso_manage/__pycache__
 python3 ./manage.py makemigrations users rso_manage events
 python3 ./manage.py migrate --run-syncdb
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@maxweis.com', 'admin')" | python3 ./manage.py shell
