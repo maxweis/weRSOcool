3424bfc7422743c1ce1ad68a6139881256bc903b
Max Weis <maxrobweis@gmail.com>
2019-04-07 07:07:06 -0500

    update event urls for new events page

--- events/urls.py
+++ events/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import include
 from . import views
 
 urlpatterns = [
+    path('events/', views.list_all_events, name='event_home'),
     path('rsos/<rso_name>/add_event', views.AddEvent, name='add_event'),
     path('rsos/<rso_name>/events', views.display_events, name='list_events'),
     path('rsos/<rso_name>/<event>/attend', views.attend_event, name='attend_event'),
