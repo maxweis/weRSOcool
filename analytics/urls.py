8a5ab264181fa23de07fe89b182c14be6fe2ee25
John Wang <jzw2@illinois.edu>
2019-04-08 16:42:59 -0500

    Added Graph for colleges

--- analytics/urls.py
+++ analytics/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import include
 from . import views
 
 urlpatterns = [
+    path('rso_colleges_chart', views.rso_colleges_chart, name='rso_colleges_chart'),
     path('', views.analytics_home, name='analytics_home'),
     path('rso_users_pie_chart', views.rso_users_pie_chart, name='rso_users_pie_chart'),
 ]
--- analytics/views.py
+++ analytics/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rso_manage.models import Registrations
+from rso_manage.models import Registrations, RSO
 import pygal
 
 def rso_users_pie_chart(request):
@@ -16,3 +16,15 @@ def rso_users_pie_chart(request):
 
 def analytics_home(request):
     return render(request, "analytics_home.html")
+
+def rso_colleges_chart(request):
+    pie_chart = pygal.Pie(title="Colleges Distribution")
+
+    colleges = {}
+    for rso in RSO.objects.all():
+        colleges[rso.college_association] = colleges.get(rso.college_association, 0) + 1
+
+    for college, count in colleges.items():
+        pie_chart.add(college, count)
+
+    return pie_chart.render_django_response()
