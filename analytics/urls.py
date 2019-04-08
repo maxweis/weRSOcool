71c9cb4e951a612090880cb23598d8513568bffa
John Wang <jzw2@illinois.edu>
2019-04-08 17:38:06 -0500

    Added Another chart

--- analytics/urls.py
+++ analytics/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('rso_colleges_chart', views.rso_colleges_chart, name='rso_colleges_chart'),
     path('', views.analytics_home, name='analytics_home'),
     path('rso_users_pie_chart', views.rso_users_pie_chart, name='rso_users_pie_chart'),
+    path('users_years', views.users_years, name='users_years'),
 ]
