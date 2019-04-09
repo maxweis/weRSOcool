a10ff7888227338cd28df08cad70c1a5c03053d0
Jayam Shah <jshah59@illinois.edu>
2019-04-09 14:26:13 -0500

    Added college_association dropdown in RSO creation

--- rso_manage/forms.py
+++ rso_manage/forms.py
@@ -7,7 +7,8 @@ class RSOCreationForm(ModelForm):
     name = forms.CharField(max_length=64)
     date_established = forms.DateField(widget=DatePickerInput)
 
-    college_association = forms.CharField(max_length=64)
+    college_association = forms.ChoiceField(choices=[(x, x) for x in ['Engineering', 'FAA', 'Business', 'LAS', 'ACES']])
+    # college_association = forms.CharField(max_length=64)
     icon = forms.ImageField(required=False)
     description = forms.CharField(widget=forms.Textarea(attrs={'rows': 3, 'cols':40}), max_length=1024)
 
