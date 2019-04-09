2f1e3bd5d53f301ffcc30bbace600338d0098d4f
Max Weis <maxrobweis@gmail.com>
2019-04-09 19:27:22 +0000

    update reset file to include deleting migrations of analytics app

--- reset_database.sh
+++ reset_database.sh
@@ -3,10 +3,12 @@ rm ./db.sqlite3
 rm -rf ./users/migrations
 rm -rf ./rso_manage/migrations
 rm -rf ./events/migrations
+rm -rf ./analytics/migrations
 rm -rf ./wersocool/__pycache__
 rm -rf ./users/__pycache__
 rm -rf ./events/__pycache__
 rm -rf ./rso_manage/__pycache__
+rm -rf ./analytics/__pycache__
 python3 ./manage.py makemigrations users rso_manage events analytics
 python3 ./manage.py migrate --run-syncdb
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@maxweis.com', 'admin')" | python3 ./manage.py shell
