7e931312f15575375574c72d0211784d4866fae4
John Wang <jzw2@illinois.edu>
2019-04-12 17:19:11 -0500

    Updated chart

--- events/urls.py
+++ events/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import include
 from . import views
 
 urlpatterns = [
+    path('rsos/<rso_name>/events/attendance_chart', views.attendance_chart, name='attendance_chart'),
     path('events/', views.list_all_events, name='event_home'),
     path('rsos/<rso_name>/add_event', views.AddEvent, name='add_event'),
     path('rsos/<rso_name>/events', views.display_events, name='list_events'),
