560bbf49b3a8b3074e97eb63766b454386fc1a01
Max Weis <maxrobweis@gmail.com>
2019-04-13 10:54:03 +0000

    add all colleges

--- rso_manage/forms.py
+++ rso_manage/forms.py
@@ -3,11 +3,24 @@ from django.forms import ModelForm
 from bootstrap_datepicker_plus import DatePickerInput
 from .models import RSO, Tag
 
+COLLEGES = [
+        'Agricultural, Consumer, and Environmental Sciences (ACES)',
+        'Applied Health Sciences (AHS)',
+        'Education (EDU)',
+        'Engineering (ENG)',
+        'Fine and Applied Arts (FAA)',
+        'Liberal Arts and Sciences (LAS)',
+        'Media (COM)',
+        'Division of General Studies (DGS)',
+        'Gies College of Business (BUS)',
+        'School of Social Work (SSW)',
+        ]
+
 class RSOCreationForm(ModelForm):
     name = forms.CharField(max_length=64)
     date_established = forms.DateField(widget=DatePickerInput)
 
-    college_association = forms.ChoiceField(choices=[(x, x) for x in ['Engineering', 'FAA', 'Business', 'LAS', 'ACES']])
+    college_association = forms.ChoiceField(choices=[(x, x) for x in COLLEGES])
     # college_association = forms.CharField(max_length=64)
     icon = forms.ImageField(required=False)
     description = forms.CharField(widget=forms.Textarea(attrs={'rows': 3, 'cols':40}), max_length=1024)
