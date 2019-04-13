216751ef9686f2b4cafaac72af9cf547740233f1
Max Weis <maxrobweis@gmail.com>
2019-04-13 04:42:57 -0500

    improve url consistency

--- wersocool/urls.py
+++ wersocool/urls.py
@@ -13,10 +13,11 @@ from analytics import urls as analytics_urls
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('signup/', users_views.SignUp, name='signup'),
-    path('login/', auth_views.LoginView.as_view(), {'template_name': 'login.html'}, name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), {'template_name': 'logged_out.html'}, name='logout'),
-    path('admin/', admin.site.urls),
+    path('signup', users_views.SignUp, name='signup'),
+    path('login', auth_views.LoginView.as_view(), {'template_name': 'login.html'}, name='login'),
+    path('logout', auth_views.LogoutView.as_view(), {'template_name': 'logged_out.html'}, name='logout'),
+    path('admin', admin.site.urls),
+    path('about_us', TemplateView.as_view(template_name='about_us.html'), name='about_us'),
     path('users/', include(users_urls)),
     path('rsos/', include(rso_urls)),
     path('analytics/', include(analytics_urls)),
